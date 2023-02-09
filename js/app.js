4e38756371ffeb376a2686a163757ec62db46a6b
Simon <83026016+simharuk2021@users.noreply.github.com>
2023-02-09 15:56:15 +0000

    media query for sidebars sticky navbar

--- js/app.js
+++ js/app.js
@@ -9,9 +9,19 @@ $(document).ready(function(){
   });
 
   // function which animates the hamburger on user click
+
 var $hamburger = $(".hamburger");
 $hamburger.on("click", function (e) {
   $hamburger.toggleClass("is-active");
+});
+
+
+$(document).ready(function () {
+  $(".slide-toggle").click(function () {
+    $(".sidebar").animate({
+      width: "toggle"
+    });
+  });
 });
 
 $(document).ready(function () {
@@ -21,3 +31,21 @@ $(document).ready(function () {
     });
   });
 });
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {navSticky()};
+
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function navSticky() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+} 
