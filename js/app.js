583f0c45e34515fe2f6b9a47ba56039d9eda3365
Simon <83026016+simharuk2021@users.noreply.github.com>
2023-02-13 10:07:45 +0000

    margin collapse implemented

--- js/app.js
+++ js/app.js
@@ -1,17 +1,17 @@
-$(document).ready(function(){
-    $(".owl-carousel").owlCarousel({
-      autoplay:true,
-      autoplayTimeout:5000,
-      autoplayHoverPause:true,
-      loop:true,
-      margin:10,
-      nav:false,
-      dots:true,
-      items: 1,
-    });
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel({
+    autoplay: true,
+    autoplayTimeout: 5000,
+    autoplayHoverPause: true,
+    loop: true,
+    margin: 10,
+    nav: false,
+    dots: true,
+    items: 1,
   });
+});
 
-  // function which animates the hamburger on user click
+// function which animates the hamburger on user click
 
 var $hamburger = $(".hamburger");
 $hamburger.on("click", function (e) {
@@ -29,14 +29,17 @@ $(document).ready(function () {
 
 $(document).ready(function () {
   $(".slide-toggle").click(function () {
+    $(".main-content").toggleClass(
+      "sidebar-open"
+    );
     $(".sidebar-full").animate({
       width: "toggle"
-    });
+    }, 300);
   });
 });
 
 // When the user scrolls the page, execute myFunction
-window.onscroll = function() {navSticky()};
+window.onscroll = function () { navSticky() };
 
 // Get the navbar
 var navbar = document.getElementById("navbar");
@@ -51,19 +54,19 @@ function navSticky() {
   } else {
     navbar.classList.remove("sticky");
   }
-} 
+}
 
-function setCookie(cname="netmatters", cvalue="today", exdays) {
+function setCookie(cname = "netmatters", cvalue = "today", exdays) {
   const d = new Date();
-  d.setTime(d.getTime() + (exdays*24*60*60*1000));
-  let expires = "expires="+ d.toUTCString();
+  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+  let expires = "expires=" + d.toUTCString();
   document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 
   closeModal();
 }
 
 
-	
+
 // var name = 'username';
 // var value = localStorage.getItem(name);
 // if(value == null) {
