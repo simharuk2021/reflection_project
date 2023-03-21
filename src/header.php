bab9a35f00c04580598fc0b79cb2fdc979a76b3f
Simon <83026016+simharuk2021@users.noreply.github.com>
2023-03-21 15:42:18 +0000

    navigation added to header

--- src/header.php
+++ src/header.php
@@ -37,6 +37,224 @@ echo '<header class="header">
         <input class="mobile-search" type="search" placeholder="Search...">
     </div>
 </div>
-</header>';
+</header>
+<section class="navigation" id="navbar">
+<div class="container">
+    <div class="navigation-wrapper">
+        <div class="nav-item">
+            <a href="#" class="navigation-item nav-color-1">
+                <div class="navigation-icon nav_item_1 nav-color-1">
+                    <i class="fas fa-th "></i>
+                </div>
+                <small class="nav-text-1">BESPOKE</small>
+                <p class="nav-text-2">SOFTWARE</p>
+            </a>
+            <!-- bespoke services overlaybanner which displays on hover -->
+            <div class="sub-banner ban-color-1">
+                <ul class=" sub-menu-nav container">
+                    <li class="sub-banner-title">
+                        <h2>Our Bespoke Software Services</h2>
+                    </li>
+                    <li class="sub-items"><a class="sub-item-link " href="#">Bespoke CRM</a></li>
+                    <li class="sub-items"><a class="sub-item-link" href="#">Mobile App
+                            Development</a></li>
+                    <li class="sub-items"><a class="sub-item-link" href="#">Operational Systems</a>
+                    </li>
+                    <li class="sub-items"><a class="sub-item-link" href="#">Intranet Development</a>
+                    </li>
+                    <li class="sub-items"><a class="sub-item-link" href="#">Business Automation</a>
+                    </li>
+                    <li class="sub-items"><a class="sub-item-link" href="#">Bespoke Databases</a>
+                    </li>
+                    <li class="sub-items"><a class="sub-item-link" href="#">Business Central
+                            Implementation</a></li>
+                    <li class="sub-items"><a class="sub-item-link" href="#">Customer Portal
+                            Development</a>
+                    </li>
+                    <li class="sub-items"><a class="sub-item-link" href="#">Software
+                            Integrations</a></li>
+                    <li class="sub-items"><a class="sub-item-link" href="#">Sharepoint
+                            Development</a></li>
+                    <li class="sub-items"><a class="sub-item-link" href="#">Internet of Things (IoT)
+                            Software</a></li>
+                    <li class="sub-items"><a class="sub-item-link" href="#">Reporting Hub</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="nav-item">
+            <a href="#" class="navigation-item nav-color-2">
+                <div class="navigation-icon nav_item_2 nav-color-2">
+                    <i class="fa-solid fa-desktop"></i>
+                </div>
+                <small class="nav-text-1">IT</small>
+                <p class="nav-text-2">SUPPORT</p>
+            </a>
+            <!-- IT Support overlaybanner which displays on hover -->
+            <div class="sub-banner ban-color-2">
+                <ul class=" sub-menu-nav container">
+                    <li class="sub-banner-title">
+                        <h2>Our IT Support Services</h2>
+                    </li>
+                    <li class="sub-items"><a class="sub-item-link" href="#">Managed IT Support</a>
+                    </li>
+                    <li class="sub-items"><a class="sub-item-link" href="#">IT Consultancy</a></li>
+                    <li class="sub-items"><a class="sub-item-link" href="#">Business IT Support</a>
+                    </li>
+                    <li class="sub-items"><a class="sub-item-link" href="#">Cloud Service
+                            Provider</a></li>
+                    <li class="sub-items"><a class="sub-item-link" href="#">Office 365 for
+                            Business</a></li>
+                    <li class="sub-items"><a class="sub-item-link" href="#">Data Backup & Disaster
+                            Recovery</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="nav-item">
+            <a href="#" class="navigation-item nav-color-3">
+                <div class="navigation-icon nav_item_3 nav-color-3">
+                    <i class="fa-solid fa-chart-simple"></i>
+                </div>
+                <small class="nav-text-1">DIGITAL</small>
+                <p class="nav-text-2">MARKETING</p>
+            </a>
+            <!-- digital marketing overlaybanner which displays on hover -->
+            <div class="sub-banner ban-color-3">
+                <ul class=" sub-menu-nav container">
+                    <li class="sub-banner-title">
+                        <h2>Our Digital Marketing Services</h2>
+                    </li>
+                    <li class="sub-items"><a class="sub-item-link" href="#">Search Engine
+                            Optimisation
+                            (SEO)</a></li>
+                    <li class="sub-items"><a class="sub-item-link" href="#">Email Marketing</a></li>
+                    <li class="sub-items"><a class="sub-item-link" href="#">Pay Per Click
+                            Advertising
+                            (PPC)</a></li>
+                    <li class="sub-items"><a class="sub-item-link" href="#">Social Media
+                            Marketing</a></li>
+                    <li class="sub-items"><a class="sub-item-link" href="#">Conversion Rate
+                            Optimisation</a>
+                    </li>
+                    <li class="sub-items"><a class="sub-item-link" href="#">Content Marketing</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="nav-item">
+            <a href="#" class="navigation-item nav-color-4">
+                <div class="navigation-icon nav_item_4 nav-color-4">
+                    <i class="fa-solid fa-phone-volume"></i>
+                </div>
+                <small class="nav-text-1">TELECOMS</small>
+                <p class="nav-text-2">SERVICES</p>
+            </a>
+            <!-- telecoms services overlaybanner which displays on hover -->
+            <div class="sub-banner ban-color-4">
+                <ul class=" sub-menu-nav container">
+                    <li class="sub-banner-title">
+                        <h2>Our Telecoms Services</h2>
+                    </li>
+                    <li class="sub-items"><a class="sub-item-link" href="#">Business Mobile</a></li>
+                    <li class="sub-items"><a class="sub-item-link" href="#">Business Broadband</a>
+                    </li>
+                    <li class="sub-items"><a class="sub-item-link" href="#">Hosted VoIP Provider</a>
+                    </li>
+                    <li class="sub-items"><a class="sub-item-link" href="#">Leased Lines
+                            Provider</a></li>
+                    <li class="sub-items"><a class="sub-item-link" href="#">Business VoIP
+                            Systems</a></li>
+                    <li class="sub-items"><a class="sub-item-link" href="#">3CX Systems</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="nav-item">
+            <a href="#" class="navigation-item nav-color-5">
+                <div class="navigation-icon nav_item_5 nav-color-5">
+                    <i class="fa-solid fa-code"></i>
+                </div>
+                <small class="nav-text-1">WEB</small>
+                <p class="nav-text-2">DESIGN</p>
+            </a>
+            <!-- web design overlaybanner which displays on hover -->
+            <div class="sub-banner ban-color-5">
+                <ul class=" sub-menu-nav container">
+                    <li class="sub-banner-title">
+                        <h2>Our Web Design Services</h2>
+                    </li>
+                    <li class="sub-items"><a class="sub-item-link" href="#">Bespoke Website
+                            Design</a></li>
+                    <li class="sub-items"><a class="sub-item-link" href="#">Branding & Design</a>
+                    </li>
+                    <li class="sub-items"><a class="sub-item-link" href="#">eCommerce Website
+                            Design</a>
+                    </li>
+                    <li class="sub-items"><a class="sub-item-link" href="#">Mobile App
+                            Development</a></li>
+                    <li class="sub-items"><a class="sub-item-link" href="#">Pay Monthly Websites</a>
+                    </li>
+                    <li class="sub-items"><a class="sub-item-link" href="#">Web Hosting</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="nav-item">
+            <a href="#" class="navigation-item nav-color-6">
+                <div class="navigation-icon nav_item_6 nav-color-6">
+                    <i class="fas fa-shield-alt"></i>
+                </div>
+                <small class="nav-text-1">CYBER</small>
+                <p class="nav-text-2">SECURITY</p>
+            </a>
+            <!-- cyber security overlaybanner which displays on hover -->
+            <div class="sub-banner ban-color-6">
+                <ul class=" sub-menu-nav container">
+                    <li class="sub-banner-title">
+                        <h2>Our Cyber Security Services</h2>
+                    </li>
+                    <li class="sub-items"><a class="sub-item-link" href="#">Cyber Security
+                            Assessmemt</a>
+                    </li>
+                    <li class="sub-items"><a class="sub-item-link" href="#">Cyber Essentials
+                            Certification</a></li>
+                    <li class="sub-items"><a class="sub-item-link" href="#">Cyber Security
+                            Management</a>
+                    </li>
+                    <li class="sub-items"><a class="sub-item-link" href="#">PCI Compliance</a></li>
+                    <li class="sub-items"><a class="sub-item-link" href="#">Cyber Penetration
+                            Testing</a>
+                    </li>
+                    <li class="sub-items"><a class="sub-item-link" href="#">Hacking Prevention</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="nav-item">
+            <a href="#" class="navigation-item nav-color-7">
+                <div class="navigation-icon nav_item_7 nav-color-7">
+                    <i class="fa-solid fa-graduation-cap"></i>
+                </div>
+                <small class="nav-text-1">DEVELOPER</small>
+                <p class="nav-text-2">COURSE</p>
+            </a>
+            <!-- developer course overlaybanner which displays on hover -->
+            <div class="sub-banner ban-color-7">
+                <ul class=" sub-menu-nav container">
+                    <li class="sub-banner-title">
+                        <h2>Our Developer Course Services</h2>
+                    </li>
+                    <li class="sub-items"><a class="sub-item-link" href="#">Train for a Career in
+                            Tech</a>
+                    </li>
+                    <li class="sub-items"><a class="sub-item-link" href="#">Scion Collaborators</a>
+                    </li>
+                    <li class="sub-items"><a class="sub-item-link" href="#">Skills Bootcamp</a></li>
+                    <li class="sub-items"><a class="sub-item-link" href="#">Scion Scheme Frequently
+                            Asked
+                            Questions</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+</section>';
 ?>
 
