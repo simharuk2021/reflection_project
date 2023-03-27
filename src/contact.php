0d3c8f00d0515a5d129652fa9bdec8f910ad37de
Simon <83026016+simharuk2021@users.noreply.github.com>
2023-03-27 12:37:52 +0100

    some card stying changes made

--- src/contact.php
+++ src/contact.php
@@ -3,14 +3,35 @@ include('header.php');
 
 ?>
 <html>
-<div>
     <div class  = "container location-cont">
         <ul class = "location-list">
-        <li class = "bold"><a href = "../index.php">Home / &nbsp;</a></li>
-        <li>Our Offices</li>
-        <ul>
+            <li class = "bold"><a href = "../index.php">Home / &nbsp;</a></li>
+            <li>Our Offices</li>
+            <ul>
     </div>
-</div>
+    <div class = "container">
+        <div class ="location-card">
+            <img class = "location-card-img">
+            <div class = "location-card-content">
+            <h2 class = "location-card-header"><a>London Office</a></h2>
+            <p class = "location-card-text">Unit G6,
+                <br>
+            Pixel Business Centre,
+                <br>
+            110 Brooker Road, Waltham Abbey
+                <br>
+            London
+                <br>
+            EN9 1JH
+            </p>
+            <h3 class = "location-card-phone">02045 397354<h3>
+            <button class = "btn btn-contact"> VIEW MORE</button>
+            </div>
+            <div class="location-card-map">
+
+            </div>
+        </div>
+    </div>
 
 
 </html>
