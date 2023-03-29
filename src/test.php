a3d8645111a7976a2d0b8ead682aa089bbeaaba9
Simon <83026016+simharuk2021@users.noreply.github.com>
2023-03-29 11:49:51 +0100

    associative array query developed

--- src/test.php
+++ src/test.php
@@ -7,11 +7,42 @@ try{
     throw new PDOException($e->getMessage());
 }
 
-$query = "SELECT * FROM details";
-$statement = $pdo->query($query);
+// $query = "SELECT id, Title, Paragraph, Image, Posted_By, Date FROM details where id = 5";
+// $statement = $pdo->query($query);
 
-while ($row = $statement->fetch()) {
-    echo $row['Title'];
-}
+// while ($row = $statement->fetch()) {
+//     echo $row['Title'];
+//     echo $row['Paragraph'];
+//     echo $row['Image'];
+// }
+
+$statement = $pdo->prepare("SELECT ID, Title, Paragraph, Image, Posted_By, Date  FROM details");
+$statement->execute();
+
+
+$result = $statement->fetch(PDO::FETCH_ASSOC);
+echo("RESULT 1 ");
+echo($result['Title']) ;
+echo($result['Paragraph']);
+echo($result['Image']);
+echo($result['Posted_By']);
+echo($result['Date']);
+
+$result2 = $statement->fetch(PDO::FETCH_ASSOC);
+echo("RESULT 2 ");
+echo($result2['Title']);
+echo($result2['Paragraph']);
+echo($result2['Image']);
+echo($result2['Posted_By']);
+echo($result2['Date']);
+
+$result3 = $statement->fetch(PDO::FETCH_ASSOC);
+echo("RESULT 3 ");
+echo($result3['Title']);
+echo($result3['Paragraph']);
+echo($result3['Image']);
+echo($result3['Posted_By']);
+echo($result3['Date']);
 
 ?>
+
