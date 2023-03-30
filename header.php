65c39c6c4568bb0d417c32f941468ec481b49432
Simon <83026016+simharuk2021@users.noreply.github.com>
2023-03-30 17:46:49 +0100

    php code restructure

--- header.php
+++ header.php
@@ -1,31 +1,285 @@
 <?php
 echo '
-<head>
-<meta charset="UTF-8">
-<meta name="description" content="netmatters">
-<script src="https://code.jquery.com/jquery-3.6.3.min.js"
-integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU=" crossorigin="anonymous"></script>
-<meta name="viewport" content="width=device-width,initial-scale=1.0">
-<link rel="shortcut icon" href="img/favicon.ico">
-<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
-<link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet">
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="description" content="netmatters">
+        <script src="https://kit.fontawesome.com/271f4647db.js" crossorigin="anonymous"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
 
-<title>Netmatters|Full Service Digital Agency|Cambridgeshire & Norfolk| Netmatters </title>
-<link rel="stylesheet"
-    href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css"
-    integrity="sha512-tS3S5qG0BlhnQROyJXvNjeEM4UpMXHrQfTGmbQ1gKmelCxlSEBUaxhRBj/EFTzpbP4RVSrpEikbmdJobCvhE3g=="
-    crossorigin="anonymous" referrerpolicy="no-referrer" />
-<link rel="stylesheet"
-    href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.css"
-    integrity="sha512-OTcub78R3msOCtY3Tc6FzeDJ8N9qvQn1Ph49ou13xgA9VsH9+LRxoFU6EqLhW4+PKRfU+/HReXmSZXHEkpYoOA=="
-    crossorigin="anonymous" referrerpolicy="no-referrer" />
-<link href="css/normalize.css" rel="stylesheet">
-<link rel="stylesheet" href="css/style.css">
-<link rel="stylesheet" href="css/hamburgers.css">
-<link rel="stylesheet" href="css/contact.css">
-</head>
+        <meta name="viewport" content="width=device-width,initial-scale=1.0">
+        <link rel="shortcut icon" href="img/favicon.ico">
+        <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+            integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+        <title>Netmatters|Full Service Digital Agency|Cambridgeshire & Norfolk| Netmatters </title>
+        <link rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css"
+            integrity="sha512-tS3S5qG0BlhnQROyJXvNjeEM4UpMXHrQfTGmbQ1gKmelCxlSEBUaxhRBj/EFTzpbP4RVSrpEikbmdJobCvhE3g=="
+            crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <link rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.css"
+            integrity="sha512-OTcub78R3msOCtY3Tc6FzeDJ8N9qvQn1Ph49ou13xgA9VsH9+LRxoFU6EqLhW4+PKRfU+/HReXmSZXHEkpYoOA=="
+            crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <link href="css/normalize.css" rel="stylesheet">
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="css/hamburgers.css">
+    </head>
+
+    <body>
+        <!-- sidebar which displays on the full view  -->
+        <div class="sidebar-full">
+            <div class="sub-menu-2 sideft-1">
+                <a href="#" class="sidebar-title-2 sidef-1">&nbsp;&nbsp;&nbsp;&nbsp;services</a>
+                <ul>
+                    <li>&nbsp;&nbsp;<i class="fas fa-angle-double-right fa-xs">&nbsp;&nbsp;&nbsp;&nbsp;</i>bespoke
+                        services</li>
+                    <li>&nbsp;&nbsp;<i class="fas fa-angle-double-right fa-xs">&nbsp;&nbsp;&nbsp;&nbsp;</i>IT Support
+                    </li>
+                    <li>&nbsp;&nbsp;<i class="fas fa-angle-double-right fa-xs">&nbsp;&nbsp;&nbsp;&nbsp;</i>Digital
+                        Marketing</li>
+                    <li>&nbsp;&nbsp;<i class="fas fa-angle-double-right fa-xs">&nbsp;&nbsp;&nbsp;&nbsp;</i>Telecoms
+                        Services</li>
+                    <li>&nbsp;&nbsp;<i class="fas fa-angle-double-right fa-xs">&nbsp;&nbsp;&nbsp;&nbsp;</i>web design
+                    </li>
+                    <li>&nbsp;&nbsp;<i class="fas fa-angle-double-right fa-xs">&nbsp;&nbsp;&nbsp;&nbsp;</i>cyber
+                        security</li>
+                </ul>
+            </div>
+            <div class="sub-menu-2 sideft-2">
+                <a href="#" class="sidebar-title-2 sidef-2">&nbsp;&nbsp;&nbsp;&nbsp;our work</a>
+                <ul>
+                    <li>&nbsp;&nbsp;<i class="fas fa-angle-double-right fa-xs">&nbsp;&nbsp;&nbsp;&nbsp;</i>Case studies
+                    </li>
+                </ul>
+            </div>
+            <div class="sub-menu-2 sideft-3">
+                <a href="#" class="sidebar-title-2 sidef-3">&nbsp;&nbsp;&nbsp;&nbsp;events</a>
+                <ul>
+                    <li>&nbsp;&nbsp;<i class="fas fa-angle-double-right fa-xs">&nbsp;&nbsp;&nbsp;&nbsp;</i>How to
+                        generate more leads through
+                        your website</li>
+
+                </ul>
+            </div>
+            <div class="sub-menu-2 sideft-4">
+                <a href="#" class="sidebar-title-2 sidef-4">&nbsp;&nbsp;&nbsp;&nbsp;our knowledge</a>
+                <ul>
+                    <li>&nbsp;&nbsp;<i class="fas fa-angle-double-right fa-xs">&nbsp;&nbsp;&nbsp;&nbsp;</i>Technologies
+                    </li>
+                    <li>&nbsp;&nbsp;<i class="fas fa-angle-double-right fa-xs">&nbsp;&nbsp;&nbsp;&nbsp;</i>Industries
+                    </li>
+                    <li>&nbsp;&nbsp;<i class="fas fa-angle-double-right fa-xs">&nbsp;&nbsp;&nbsp;&nbsp;</i>News</li>
+                    <li>&nbsp;&nbsp;<i class="fas fa-angle-double-right fa-xs">&nbsp;&nbsp;&nbsp;&nbsp;</i>Insights</li>
+                </ul>
+            </div>
+            <div class="sub-menu-2 sideft-5">
+                <a href="#" class="sidebar-title-2 sidef-5">&nbsp;&nbsp;&nbsp;&nbsp;training</a>
+                <ul>
+                    <li>&nbsp;&nbsp;<i class="fas fa-angle-double-right fa-xs">&nbsp;&nbsp;&nbsp;&nbsp;</i>Train for a
+                        career in tech</li>
+                    <li>&nbsp;&nbsp;<i class="fas fa-angle-double-right fa-xs">&nbsp;&nbsp;&nbsp;&nbsp;</i>skills
+                        bootcamp</li>
+                    <li>&nbsp;&nbsp;<i class="fas fa-angle-double-right fa-xs">&nbsp;&nbsp;&nbsp;&nbsp;</i>scs
+                        frequently asked
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;questions</li>
+                    <li>&nbsp;&nbsp;<i class="fas fa-angle-double-right fa-xs">&nbsp;&nbsp;&nbsp;&nbsp;</i>scion
+                        collaborators</li>
+                </ul>
+            </div>
+            <div class="sub-menu-2 sideft-6">
+                <a href="#" class="sidebar-title-2 sidef-6">&nbsp;&nbsp;&nbsp;&nbsp;our company</a>
+                <ul>
+                    <li>&nbsp;&nbsp;<i class="fas fa-angle-double-right fa-xs">&nbsp;&nbsp;&nbsp;&nbsp;</i>why choose
+                        us?</li>
+                    <li>&nbsp;&nbsp;<i class="fas fa-angle-double-right fa-xs">&nbsp;&nbsp;&nbsp;&nbsp;</i>our culture
+                    </li>
+                    <li>&nbsp;&nbsp;<i class="fas fa-angle-double-right fa-xs">&nbsp;&nbsp;&nbsp;&nbsp;</i>our team</li>
+                    <li>&nbsp;&nbsp;<i class="fas fa-angle-double-right fa-xs">&nbsp;&nbsp;&nbsp;&nbsp;</i>our careers
+                    </li>
+                    <li>&nbsp;&nbsp;<i class="fas fa-angle-double-right fa-xs">&nbsp;&nbsp;&nbsp;&nbsp;</i>our benefits
+                    </li>
+                </ul>
+            </div>
+
+            <div class="sub-menu-2 sideft-7">
+                <a href="#" class="sidebar-title-2 sidef-7">&nbsp;&nbsp;&nbsp;&nbsp;contact us</a>
+                <ul>
+                    <li>&nbsp;&nbsp;<i class="fas fa-angle-double-right fa-xs">&nbsp;&nbsp;&nbsp;&nbsp;</i>London Office
+                    </li>
+                    <li>&nbsp;&nbsp;<i class="fas fa-angle-double-right fa-xs">&nbsp;&nbsp;&nbsp;&nbsp;</i>Cambridge
+                        Office</li>
+                    <li>&nbsp;&nbsp;<i class="fas fa-angle-double-right fa-xs">&nbsp;&nbsp;&nbsp;&nbsp;</i>Wymondham
+                        Office</li>
+                    <li>&nbsp;&nbsp;<i class="fas fa-angle-double-right fa-xs">&nbsp;&nbsp;&nbsp;&nbsp;</i>Great
+                        Yarmouth Office</li>
+                </ul>
+            </div>
+        </div>
+        <!-- sidebar which displays for the mobile view -->
+        <div class="sidebar">
+            <div class="sidebar-inner">
+                <a class="sidebar-title-top" href="#">CONTACT US</a>
+                <div class="sub-menu" id="subm-1">
+                    <a class="sidebar-title" id="subt-1" href="#">
+                        <div class="nav-item-1"><i class="fas fa-th"></i></div> <small>Bespoke</small>Software
+                    </a>
+                    <ul>
+                        <li>bespoke CRM</li>
+                        <li>business automation</li>
+                        <li>software integrations</li>
+                        <li>mobile app development</li>
+                        <li>bespoke databases</li>
+                        <li>sharepoint development</li>
+                        <li>operational systems</li>
+                        <li>business central implementation</li>
+                        <li>internet of things (iot) software</li>
+                        <li>intranet development</li>
+                        <li>customer portal development</li>
+                        <li>reporting hub</li>
+                    </ul>
+                </div>
+                <div class="sub-menu" id="subm-2">
+                    <a href="#" class="sidebar-title" id="subt-2">
+                        <div class="nav-item-2"><i class="fa-solid fa-desktop"></i></div><small>IT</small>Support
+                    </a>
+                    <ul>
+                        <li>managed it support</li>
+                        <li>business it support</li>
+                        <li>office 365 for business</li>
+                        <li>it consultancy</li>
+                        <li>cloud service provider</li>
+                        <li>data backup & disaster recovery</li>
+                    </ul>
+                </div>
+                <div class="sub-menu" id="subm-2a">
+                    <a href="#" class="sidebar-title" id="subt-2a">
+                        <div class="nav-item-3"><i class="fa-solid fa-chart-simple"></i></div>
+                        <small>DIGITAL</small>MARKETING
+                    </a>
+                    <ul>
+                        <li>search engine optimisation (SEO)</li>
+                        <li>pay per click advertising (ppc)</li>
+                        <li>conversion rate optimisation (cro)</li>
+                        <li>email marketing</li>
+                        <li>social media marketing</li>
+                        <li>content marketing</li>
+                    </ul>
+                </div>
+                <div class="sub-menu" id="subm-3">
+                    <a href="#" class="sidebar-title" id="subt-3">
+                        <div class="nav-item-4"> <i class="fa-solid fa-phone-volume"></i></div>
+                        <small>telecoms</small>services
+                    </a>
+                    <ul>
+                        <li>business mobile</li>
+                        <li>hosted voip provider</li>
+                        <li>business voip systems</li>
+                        <li>business broadband</li>
+                        <li>leased lines provider</li>
+                        <li>3cx systems</li>
+                    </ul>
+                </div>
+                <div class="sub-menu" id="subm-4">
+                    <a href="#" class="sidebar-title" id="subt-4">
+                        <div class="nav-item-5"> <i class="fa-solid fa-code"></i></div><small>web</small>design
+                    </a>
+                    <ul>
+                        <li>bespoke website design</li>
+                        <li>ecommerce website design</li>
+                        <li>pay monthly websites</li>
+                        <li>branding and design</li>
+                        <li>mobile app development</li>
+                        <li>web hosting</li>
+                    </ul>
+                </div>
+                <div class="sub-menu" id="subm-5">
+                    <a href="#" class="sidebar-title" id="subt-5">
+                        <div class="nav-item-6"><i class="fas fa-shield-alt"></i></div><small>Cyber</small>security
+                    </a>
+                    <ul>
+                        <li>cyber security assessment</li>
+                        <li>cyber security management</li>
+                        <li>cyber penetration testing</li>
+                        <li>cyber essentials certification</li>
+                        <li>pci compliance</li>
+                        <li>hacking prevention</li>
+                    </ul>
+                </div>
+                <div class="sub-menu" id="subm-6">
+                    <a href="#" class="sidebar-title" id="subt-6">
+                        <div class="nav-item-7"> <i class="fa-solid fa-graduation-cap"></div>
+                        </i><small>developer</small>course
+                    </a>
+                    <ul>
+                        <li>train for a career in tech</li>
+                        <li>skills bootcamp</li>
+                        <li>scion scheme frequently asked questions</li>
+                        <li>scion collaborators</li>
+                    </ul>
+                </div>
+            </div>
+            <a href="#" class="sidebar-title-2">Our work</a>
+            <div class="sub-menu-2">
+                <ul>
+                    <li>&nbsp;&nbsp;<i class="fas fa-angle-double-right fa-xs"></i> case studies</li>
+                </ul>
+            </div>
+            <a href="#" class="sidebar-title-2">Events</a>
+            <div class="sub-menu-2">
+                <ul>
+                    <li>&nbsp;&nbsp;<i class="fas fa-angle-double-right fa-xs"></i> How to generate more
+                        &nbsp;&nbsp;&nbsp;&nbsp;leads through your &nbsp;&nbsp;&nbsp;&nbsp;website</li>
+                </ul>
+            </div>
+            <a href="#" class="sidebar-title-2">our knowledge</a>
+            <div class="sub-menu-2">
+                <ul>
+                    <li>&nbsp;&nbsp;<i class="fas fa-angle-double-right fa-xs"></i> Technologies</li>
+                    <li>&nbsp;&nbsp;<i class="fas fa-angle-double-right fa-xs"></i> Industries</li>
+                    <li>&nbsp;&nbsp;<i class="fas fa-angle-double-right fa-xs"></i> News</li>
+                    <li>&nbsp;&nbsp;<i class="fas fa-angle-double-right fa-xs"></i> Insights</li>
+                </ul>
+            </div>
+            <a href="#" class="sidebar-title-2">training</a>
+            <div class="sub-menu-2">
+                <ul>
+                    <li>&nbsp;&nbsp;<i class="fas fa-angle-double-right fa-xs"></i> Train for a career in
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;tech</li>
+                    <li>&nbsp;&nbsp;<i class="fas fa-angle-double-right fa-xs"></i> skills bootcamp</li>
+                    <li>&nbsp;&nbsp;<i class="fas fa-angle-double-right fa-xs"></i> scs frequently asked
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;questions</li>
+                    <li>&nbsp;&nbsp;<i class="fas fa-angle-double-right fa-xs"></i> scion collaborators</li>
+                </ul>
+            </div>
+            <a href="#" class="sidebar-title-2">our company</a>
+            <div class="sub-menu-2">
+                <ul>
+                    <li>&nbsp;&nbsp;<i class="fas fa-angle-double-right fa-xs"></i> why choose us?</li>
+                    <li>&nbsp;&nbsp;<i class="fas fa-angle-double-right fa-xs"></i> our culture</li>
+                    <li>&nbsp;&nbsp;<i class="fas fa-angle-double-right fa-xs"></i> our team</li>
+                    <li>&nbsp;&nbsp;<i class="fas fa-angle-double-right fa-xs"></i> our careers</li>
+                    <li>&nbsp;&nbsp;<i class="fas fa-angle-double-right fa-xs"></i> our benefits</li>
+                </ul>
+            </div>
+
+            <a href="#" class="sidebar-title-2">contact us</a>
+            <div class="sub-menu-2">
+                <ul>
+                    <li>&nbsp;&nbsp;<i class="fas fa-angle-double-right fa-xs"></i> london office</li>
+                    <li>&nbsp;&nbsp;<i class="fas fa-angle-double-right fa-xs"></i> cambridge office</li>
+                    <li>&nbsp;&nbsp;<i class="fas fa-angle-double-right fa-xs"></i> wymondham office</li>
+                    <li>&nbsp;&nbsp;<i class="fas fa-angle-double-right fa-xs"></i> great yarmouth office</li>
+                    <li>&nbsp;&nbsp;<i class="fas fa-angle-double-right fa-xs"></i> our benefits</li>
+                </ul>
+            </div>
+            <a href="#" class="sidebar-title-2">covid risk assessments</a>
+            <a href="#" class="sidebar-title-2">support</a>
+        </div>
+        <div class = "main-content">
 <header class="header">
 <div class="container">
     <div class="top-row">
--- src/footer.php
+++ src/footer.php
@@ -141,7 +141,18 @@ echo'<head>
                 </div>
             </div>
 
-        </footer>'
+        </footer>
+        </div>
+        <script src="https://code.jquery.com/jquery-3.6.3.min.js"
+            integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU=" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"
+            integrity="sha512-bPs7Ae6pVvhOSiIcyUClR7/q2OAsRiovw4vAkX+zJbw3ShAeeqezq50RIIcIURq7Oa20rW2n2q+fyXBNcU9lrw=="
+            crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+        <script src="js/app.js"></script>
+
+    </body>
+
+</html>'
         ;
 
         ?>
